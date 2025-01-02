ac0f3470543e615a0a3cdfed4deee77f65d22e5f
Matt Dow <matt.dow.vo@gmail.com>
2025-01-02 09:33:08 -0600

    Improve code, space and time efficiency by checking for repeats on the fly in the recursive function. Swap elements using array destructuring rather than creating a temp variable.

--- Leetcode/no-repeats.js
+++ Leetcode/no-repeats.js
@@ -4,47 +4,40 @@
  */
 
 const noRepeats = function(str) {
-  const strArr = str.split('');
-  let permutations = [];
   let count = 0;
-
+  const strArr = str.split('');
+  
   const swap = function(arr, index1, index2) {
-    let temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2] = temp;
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]]; 
+  };
+  
+  const checkNoRepeats = function(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] === arr[i + 1]) return false;
+    }
+    return true;
   };
 
   const generatePermutations = function(n, arr) {
     if (n === 1) {
-      permutations.push(arr.join(''));
-    } else {
-      for (let i = 0; i < n - 1; i++) {
-        generatePermutations(n - 1, arr);
-        if (n % 2 === 0) {
-          swap(arr, i, n - 1);
-        } else {
-          swap(arr, 0, n - 1);
-        }
-      }
-      generatePermutations(n - 1, arr);
+      if (checkNoRepeats(arr)) count++;
+      return;
     }
+    
+    for (let i = 0; i < n - 1; i++) {
+      generatePermutations(n - 1, arr);
+      if (n % 2 === 0) {
+        swap(arr, i, n - 1);
+      } else {
+        swap(arr, 0, n - 1);
+      }
+    }
+    generatePermutations(n - 1, arr);
   };
 
   generatePermutations(strArr.length, strArr);
-  for (let i = 0; i < permutations.length; i++) {
-    let current = permutations[i];
-    let repeat = false;
-    for (let j = 0; j < current.length - 1; j++) {
-      if (current[j] === current[j + 1]) {
-        repeat = true;
-      }
-    }
-    if (!repeat) {
-      count++;
-    }
-  }
   return count;
-}
+};
 
 console.log(noRepeats('aab')); // 2
 console.log(noRepeats('aaa')); // 0
